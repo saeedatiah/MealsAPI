ad2549133b23cf1a6b4b8275ef4a6d25678595c6
unknown <saeedatiah17@gmail.com>
2024-08-30 20:16:41 +0300

    upload project

---
+++ MealsApi/Models/Meals.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MealsApi.Models
+{
+    public class Meals
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Descr { get; set; }
+        public string ImgURL { get; set; }
+        public float Price { get; set; } = 0;
+        public int CatID { get; set; }
+    }
+}
---
+++ MealsApi/Services/GeneralQuery.cs
@@ -0,0 +1,33 @@
+﻿namespace MealsApi.Services
+{
+    public static class GeneralQuery
+    {
+        public static string getAllRecords(string tbName) => "select * from "+ tbName + "";
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cols">insert list cols to show</param>
+        /// <returns></returns>
+        public static string getAllRecordsWithCustomeCols(string tbName, string[] cols) => "select "+string.Join(",",cols)+" from "+ tbName + "";
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Id">this Id must be as Id</param>
+        /// <returns></returns>
+        public static string getOneRecord(string tbName,int Id) => "select * from "+ tbName + " where Id="+ Id + "";
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cols"> columns to show </param>
+        /// <param name="Id">this Id must be as Id</param>
+        /// <returns></returns>
+        public static string getOnRecordsWithCustomeCols(string tbName, string[] cols,int Id) => "select " + string.Join(",", cols) + " from " + tbName + " where Id="+Id+"";
+
+        public static string deleteRecord(string tbName,int Id) => "delete from "+ tbName + " where Id="+ Id + "";
+        public static string insertRecord(string tbName, string[] cols, string[] values) => "insert into "+tbName+" ("+string.Join(",",cols)+") values ("+string.Join(",",values)+")";
+
+
+
+
+    }
+}
