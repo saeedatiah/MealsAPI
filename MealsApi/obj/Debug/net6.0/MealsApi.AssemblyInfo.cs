d536fab70d908b62feea420cb83104eecdcffcf7
saeedatiah <saeedatiah17@gmail.com>
2024-08-31 19:31:30 +0300

    edit

--- MealsApi/obj/Debug/net6.0/MealsApi.AssemblyInfo.cs
+++ MealsApi/obj/Debug/net6.0/MealsApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MealsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e63fef65ddf38cc82a60e2073931a7c52c04b436")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52e4dda11cd7828f4e4f69dd305fece7040d22c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MealsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MealsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
